6105876b27479f84703df014088b11a1ae140cd9
Alyssa Pratt <alyssakpratt@gmail.com>
2021-02-06 17:30:35 -0600

    small change https for security on netlify test

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ window.addEventListener('load', ()=> {
             long = position.coords.longitude;
             lat = position.coords.latitude;
 
-            const api = `http://api.weatherapi.com/v1/current.json?key=f1fa4ac4a07e48658f5220351201808&q=${lat},${long}`;
+            const api = `https://api.weatherapi.com/v1/current.json?key=f1fa4ac4a07e48658f5220351201808&q=${lat},${long}`;
 
             fetch(api)
             .then(response =>{
